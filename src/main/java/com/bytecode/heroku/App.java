64098c21ebdbe19fc031b1a87e2b8d0b168f43b8
naccata <gcannata23@gmail.com>
2022-06-10 10:42:06 +0200

    Add more output

--- src/main/java/com/bytecode/heroku/App.java
+++ src/main/java/com/bytecode/heroku/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("[START] ENVIRONMENT_VARIABLES");
         System.getenv().forEach((k, v) -> System.out.printf("%s=%s\n", k, v));
+        System.out.println("[END] ENVIRONMENT_VARIABLES");
     }
 }
